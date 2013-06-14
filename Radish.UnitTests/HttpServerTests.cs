f022e7d7be348286904870f05f358c1376c54499
YiHao Liu <zxglyh@gmail.com>
2013-06-14 09:25:57 +0800

    Merge from master

---
+++ Radish.UnitTests/HttpServerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Radish.UnitTests
+{
+    public class HttpServerTests
+    {
+        [Test]
+        public void MappingTest()
+        {
+            var server = new HttpServer();
+            server.When(request => request.Uri.Is("home/index"));
+//                  .Then(response => response.File("c:\\test.html").Header("Accept", "text/html"));
+        }
+    }
+}
